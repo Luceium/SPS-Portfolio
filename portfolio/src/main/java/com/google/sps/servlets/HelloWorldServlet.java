f69aca4eb386377b21ca05e313457b965efa4014
mauricio <mauriciom.curiel@gmail.com>
2022-06-09 22:17:41 +0000

    adds method providing an array list of messages

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,5 +1,6 @@
 package com.google.sps.servlets;
 
+import java.util.ArrayList;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,9 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
+  public ArrayList<String> getMessages(){
+    ArrayList<String> messages = new ArrayList<String>();
+    messages.add("Did you click me?");
+    messages.add("Click me again, I dare you!");
+    messages.add("Why would you click me?!");
+    return messages;
+  }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
+    
     response.getWriter().println("Hello Mau!");
   }
 }
