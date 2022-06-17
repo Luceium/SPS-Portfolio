fa0cbbd312d65ccb28e3beed42edfca6f31ee07a
mauricio <mauriciom.curiel@gmail.com>
2022-06-17 19:14:09 +0000

    updates accepted languages to demonstrate that it is static

--- portfolio/src/main/java/com/google/sps/servlets/TranslateAPIServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/TranslateAPIServlet.java
@@ -14,18 +14,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/translate")
 public class TranslateAPIServlet extends HttpServlet{
 
-    HashSet<String> acceptedLanguages = new HashSet<String>();
+    private static HashSet<String> ACCEPTED_LANGUAGES = new HashSet<String>();
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        acceptedLanguages.add("en");
-        acceptedLanguages.add("es");
-        acceptedLanguages.add("fr");
+        ACCEPTED_LANGUAGES.add("en");
+        ACCEPTED_LANGUAGES.add("es");
+        ACCEPTED_LANGUAGES.add("fr");
 
         String langCode = request.getParameter("langCode");
         //Stops code from running if there is an invalid language
-        if (!acceptedLanguages.contains(langCode)){
+        if (!ACCEPTED_LANGUAGES.contains(langCode)){
             System.out.println("Language code: " + langCode + " is invalid.");
             //resets the page to english to not destroy the page conentent
             langCode = "en";
