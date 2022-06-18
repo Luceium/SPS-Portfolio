19492228018b8416d74ee12b0993e3537a654df4
mauricio <mauriciom.curiel@gmail.com>
2022-06-18 01:12:48 +0000

    adds datastore too servlet

--- portfolio/src/main/java/com/google/sps/servlets/CommentHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentHandlerServlet.java
@@ -1,10 +1,17 @@
 package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+// import org.jsoup.Jsoup;
+// import org.jsoup.safety.Whitelist;
 
 @WebServlet("/suggestions")
 public class CommentHandlerServlet extends HttpServlet {
@@ -16,6 +23,14 @@ public class CommentHandlerServlet extends HttpServlet {
         String comment = request.getParameter("text-comment");
         String name = request.getParameter("commenter-name");
 
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("Suggestion");
+        FullEntity suggestionEntity =
+            Entity.newBuilder(keyFactory.newKey())
+                .set("name", name)
+                .set("comment", comment)
+                .build();
+        datastore.put(suggestionEntity);
         //log comment for later
         System.out.println("Commenter: " + name);
         System.out.println("Comment: " + comment);
