61beb5ba651d2ce25f5566e9391d0316a529e66f
mauricio <mauriciom.curiel@gmail.com>
2022-06-21 17:29:55 +0000

    abstracts read and write for cloud datastore

--- portfolio/src/main/java/com/google/sps/servlets/CommentHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentHandlerServlet.java
@@ -1,15 +1,13 @@
 package com.google.sps.servlets;
 
-import com.google.cloud.datastore.Datastore;
-import com.google.cloud.datastore.DatastoreOptions;
-import com.google.cloud.datastore.Entity;
-import com.google.cloud.datastore.FullEntity;
-import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.sps.apihelper.DatastoreHelper;
+
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Safelist;
 
@@ -19,20 +17,12 @@ public class CommentHandlerServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         
-        //gets comment text
+        //gets infromation
         String comment = Jsoup.clean(request.getParameter("text-comment"), Safelist.none());
         String name = request.getParameter("commenter-name");
         long timestamp = System.currentTimeMillis();
 
-        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-        KeyFactory keyFactory = datastore.newKeyFactory().setKind("Suggestion");
-        FullEntity suggestionEntity =
-            Entity.newBuilder(keyFactory.newKey())
-                .set("name", name)
-                .set("comment", comment)
-                .set("timestamp", timestamp)
-                .build();
-        datastore.put(suggestionEntity);
+        DatastoreHelper.write(name, comment, timestamp);
         
         response.sendRedirect("/index.html");
     }
--- portfolio/src/main/java/com/google/sps/servlets/SuggestionServingServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/SuggestionServingServlet.java
@@ -14,17 +14,11 @@
 
 package com.google.sps.servlets;
 
-import com.google.cloud.datastore.Datastore;
-import com.google.cloud.datastore.DatastoreOptions;
-import com.google.cloud.datastore.Entity;
-import com.google.cloud.datastore.Query;
-import com.google.cloud.datastore.QueryResults;
-import com.google.cloud.datastore.StructuredQuery.OrderBy;
 import com.google.gson.Gson;
+import com.google.sps.apihelper.DatastoreHelper;
 import com.google.sps.data.Comment;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -37,22 +31,7 @@ public class SuggestionServingServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-    Query<Entity> query =
-        Query.newEntityQueryBuilder().setKind("Suggestion").setOrderBy(OrderBy.desc("timestamp")).build();
-    QueryResults<Entity> results = datastore.run(query);
-
-    List<Comment> comments = new ArrayList<>();
-    while (results.hasNext()) {
-      Entity entity = results.next();
-
-      long id = entity.getKey().getId();
-      String commentConetent = entity.getString("comment");
-      long timestamp = entity.getLong("timestamp");
-
-      Comment comment = new Comment(id, commentConetent, timestamp);
-      comments.add(comment);
-    }
+    List<Comment> comments = DatastoreHelper.read();
 
     Gson gson = new Gson();
 
